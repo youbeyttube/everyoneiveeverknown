b0da1766d848c5d196c3c8f352c465952c0da07b
youbeyttube <youbeyttube@gmail.com>
2026-07-25 12:16:37 +0100

    Add route to serve index.html
    Added a new route to serve the index.html file.

--- backend/server.js
+++ backend/server.js
@@ -103,3 +103,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
